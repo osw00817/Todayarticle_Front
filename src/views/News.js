7c11d4ce3b7a321317b40b1b8b01df112634ef2a
osw00817 <osw00817@naver.com>
2020-02-23 16:19:33 +0900

    test

--- src/views/News.js
+++ src/views/News.js
@@ -10,11 +10,9 @@ export default function Main({location,match}) {
     console.log(location);
     console.log(match);
     console.log(match.params.name)
-    console.log(querystring.parse(location.search));
-    console.log(querystring.parse(location.search).target);
     return(
         <Fragment>
-            <News target="호날두"/>
+            <News target={querystring.parse(location.search).search}/>
         </Fragment>
     )
 }
